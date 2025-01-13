f51ec45da295a2802741280c4acfff2992634619
unknown <travihay@gmail.com>
2025-01-12 19:32:13 -0600

    changed the css html and js

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,17 @@ let cnt2 = document.getElementById("container-2");
 let cnt3 = document.getElementById("container-3");
 
 btnEl.addEventListener("click", function () {
-    localStorage.setItem("value", JSON.stringify(inputEl.value));
-    const caughtValue = JSON.parse(localStorage.getItem("value"))
-    const feetUnit = (caughtValue * 3.281).toFixed(3)
-    const meterUnit = (caughtValue / 3.281).toFixed(3)
-    const gallonUnit = (caughtValue * 0.264).toFixed(3)
-    const literUnit = (caughtValue / 0.264).toFixed(3)
-    const poundUnit = (caughtValue * 2.204).toFixed(3)
-    const kilogramUnit = (caughtValue / 2.204).toFixed(3)
+    const caughtValue = inputEl.value
+    const feetUnit = (caughtValue * 3.281).toFixed(3);
+    const meterUnit = (caughtValue / 3.281).toFixed(3);
+    const gallonUnit = (caughtValue * 0.264).toFixed(3);
+    const literUnit = (caughtValue / 0.264).toFixed(3);
+    const poundUnit = (caughtValue * 2.204).toFixed(3);
+    const kilogramUnit = (caughtValue / 2.204).toFixed(3);
 
-    cnt1.innerHTML = ` <p class="length">Length (Meter/Feet)</p>
-                                  <p class="length">${caughtValue} meters = ${feetUnit} feet | ${caughtValue} feet = ${meterUnit} meters</P>`
-    cnt2.innerHTML = ` <p class="volume">Volume (Liters/Gallons)</p>
-                                  <p class="volume">${caughtValue} liters = ${gallonUnit} gallons | ${caughtValue} gallons = ${literUnit} liters</P>`
-    cnt3.innerHTML = ` <p class="mass">Mass (Kilograms/Pounds)</p>
-                                  <p class="mass">${caughtValue} kilos = ${poundUnit} pounds | ${caughtValue} pounds = ${kilogramUnit} kilos</P>`
+    cnt1.querySelector(".measurment-data").innerHTML = `${caughtValue} meters = ${feetUnit} feet | ${caughtValue} feet = ${meterUnit} meters`
+    cnt2.querySelector(".measurment-data").innerHTML = `${caughtValue} liters = ${gallonUnit} gallons | ${caughtValue} gallons = ${literUnit} liters`
+    cnt3.querySelector(".measurment-data").innerHTML = `${caughtValue} kilos = ${poundUnit} pounds | ${caughtValue} pounds = ${kilogramUnit} kilos`
 })
+
+
